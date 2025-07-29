78d4cd2cf7a9462b95b39c19f539ff7403e3aa01
PaulinaDiaz2912 <paulina.diazb@udea.edu.co>
2025-07-29 12:27:36 -0500

    1

---
+++ Reto9/js/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    fetch('data/top_20_paises.json')
+        .then(response => response.json())
+        .then(data => {
+            const ctx 
+        })
+});
